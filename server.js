01843cd275411ae6638269d5474e5fa65924c802
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-28 14:36:55 +0300

    Create Documentation page

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(express.static("./public"))
 app.use("/categories", categories)
 app.use("/products", products)
 
+app.get('/', (req, res) => {
+    res.send('/index.html')
+})
+
 mongoose.connect(process.env.MONGO_URI).then(() => {
     app.listen(PORT, () => {
         console.log(`Server Started at port ${PORT}`)
