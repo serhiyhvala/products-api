021e497f5508836c3e57decc53b4fac363b56740
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-28 10:55:37 +0300

    Connect to Mongo DB

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 require("dotenv").config()
 const express = require("express")
+const mongoose = require("mongoose")
 const cors = require("cors")
 const PORT = process.env.PORT
 const categories = require("./routes/categories.routes")
 
 const app = express()
 app.use(cors())
-
+app.use(express.json())
 app.use("/categories", categories)
 
-app.listen(PORT, () => {
-    console.log(`Server Started at port ${PORT}`)
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server Started at port ${PORT}`)
+    })
 })
